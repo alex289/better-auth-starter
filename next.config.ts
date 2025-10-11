99cc8a316128027e2ee1f333f4f0bc13074e4138
alex289 <mccr4fthd@gmail.com>
2025-10-11 14:15:05 +0200

    feat: Nextjs 16 beta

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   typedRoutes: true,
+  reactCompiler: true,
+  experimental: {
+    turbopackFileSystemCacheForDev: true,
+  },
 };
 
 export default nextConfig;
